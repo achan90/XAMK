1f68fe3a6f88aa2fc477b461b4139545db08e9e8
Achan90 <pousi.pekka@gmail.com>
2017-09-12 18:56:55 +0300

    update

--- JavaScript/Merkkijonoja.js
+++ JavaScript/Merkkijonoja.js
@@ -30,44 +30,37 @@
 
 function kuvioni(shape){
     if(shape == 1){
-        window.alert(
-            "* * * * *\n"+ 
-            " * * * * *\n"+
-            "* * * * *\n"+
-            " * * * * *\n"+
-            "* * * * *\n");
-            return "* * * * *\n * * * * *\n* * * * * \n * * * * *\n* * * * *";
+        return  "* * * * *\n"+ 
+                " * * * * *\n"+
+                "* * * * *\n"+
+                " * * * * *\n"+
+                "* * * * *\n";
 
     }else if(shape == 2){
-        window.alert(
-            "* * * * *\n"+
-            "*         *\n"+
-            "*         *\n"+
-            "*         *\n"+
-            "* * * * *\n");
-            return "* * * * * \n*       *\n*       *\n*       *\n* * * * *";
+        return  "* * * * *\n"+
+                "*       *\n"+
+                "*       *\n"+
+                "*       *\n"+
+                "* * * * *\n";
 
     }else if(shape == 3){
-        window.alert(
-            "* * * * *\n"+
-            "  *     *\n"+
-            "   *   *\n"+
-            "    * *\n"+
-            "     *\n");
-            return "* * * * *\n *     *\n  *   *\n   * *\n    *"
+        return  " * * * * *\n"+
+                "  *     *\n"+
+                "   *   *\n"+
+                "    * *\n"+
+                "     *\n";
 
     }else if(shape == 4){
-        window.alert(
-            "  *\n"+
-            " * *\n"+
-            "*   *\n"+
-            " * *\n"+
-            "  * \n");
-            return "  *\n * *\n*   *\n * *\n  *"
+        return  "  *\n"+
+                " * *\n"+
+                "*   *\n"+
+                " * *\n"+
+                "  * \n";
+    // Vika ilmoitus eroaa tehtävän annon omasta, sillä käyttö tapahtuu window.promptin avulla.            
     }else{
-        return "*** Käyttö: kuvioni(x), missä x = 1,...4.";
+        window.alert("Virheellinen syöte, ohjelma uudelleen käynnistyy.");
+        return kuvioni(window.prompt("Syötä kuvion numero (1-4):"));
     }
 }
 
-kuvioni(4)
-//vaihtoehtoinen käyttötapa: kuvioni(window.prompt("Syötä kuvion numero (1-4):"))
+kuvioni(window.prompt("Syötä kuvion numero (1-4):"))
--- JavaScript/triangle.js
+++ JavaScript/triangle.js
@@ -1,20 +1,21 @@
 function triangle(side1, side2, side3) {
-    if (side1 == side2 && side1 != side3 || side1 == side3 && side1 != side2 || side2 == side3 && side1 != side3) {
-        window.alert("Kolmio on tasakylkinen.");
-
-    } else if (side1 && side2 == side3) {
-        window.alert("Kolmio on tasasivuinen.")
-
-    } else if (side1 != side2 && side1 != side3 && side2 != side3) {
-        window.alert("Kolmio ei ole tasasivuinen, eikä tasakylkinen.")
-    
-    //Korjaa tämä osio.
-   /* } else if (isNan(side1 || side2 || side3)) {
-        window.alert("Syötä kolmion kylkien pituudet numeroina, vitun urpo:");
+    if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+        window.alert("Virheelliset syötteet, ohjelma uudelleen käynnistyy.");
         triangle(window.prompt("Ensimmäisen kyljen pituus:"), 
-            window.prompt("Toisen kyljen pituus:"),window.prompt("Kolmannen kyljen pituus:"));*/
-}}
+            window.prompt("Toisen kyljen pituus:"),window.prompt("Kolmannen kyljen pituus:"));
+    }else{
+        if (side1 == side2 && side1 != side3 || side1 == side3 && side1 != side2 || side2 == side3 && side1 != side3) {
+            window.alert("Kolmio on tasakylkinen.");
 
-window.alert("Syötä kolmion kylkien pituudet numeroina, vitun urpo:");
+        } else if (side1 && side2 == side3) {
+            window.alert("Kolmio on tasasivuinen.")
+
+        } else if (side1 != side2 && side1 != side3 && side2 != side3) {
+            window.alert("Kolmio ei ole tasasivuinen, eikä tasakylkinen.")
+        }
+    }
+}
+
+window.alert("Syötä kolmion kylkien pituudet numeroina");
 triangle(window.prompt("Ensimmäisen kyljen pituus:"), window.prompt("Toisen kyljen pituus:"),
     window.prompt("Kolmannen kyljen pituus:"))
