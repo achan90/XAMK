eedff9e98fb986283c5847e475925c1c47405693
Achan90 <pousi.pekka@gmail.com>
2017-09-12 17:56:12 +0300

    update

--- JavaScript/triangle.js
+++ JavaScript/triangle.js
@@ -6,16 +6,16 @@ function triangle(side1, side2, side3) {
         window.prompt("Kolmio on tasasivuinen.")
 
     } else if (isNan(side1 || side2 || side3)) {
-        window.alert("Syˆt‰ kolmion kylkien pituudet numeroina, vitun urpo:");
-        triangle(window.prompt("Ensimm‰isen kyljen pituus:"), window.prompt("Toisen kyljen pituus:"),
+        window.alert("Sy√∂t√§ kolmion kylkien pituudet numeroina, vitun urpo:");
+        triangle(window.prompt("Ensimm√§isen kyljen pituus:"), window.prompt("Toisen kyljen pituus:"),
             window.prompt("Kolmannen kyljen pituus:"));
 
     } else if (side1 != side2 && side1 != side3 && side2 != side3) {
-        window.prompt("Kolmio ei ole tasasivuinen, eik‰ tasakylkinen.")
+        window.prompt("Kolmio ei ole tasasivuinen, eik√§ tasakylkinen.")
     }
 }
 
-window.alert("Syˆt‰ kolmion kylkien pituudet numeroina, vitun urpo:");
-triangle(window.prompt("Ensimm‰isen kyljen pituus:"), window.prompt("Toisen kyljen pituus:"),
+window.alert("Sy√∂t√§ kolmion kylkien pituudet numeroina, vitun urpo:");
+triangle(window.prompt("Ensimm√§isen kyljen pituus:"), window.prompt("Toisen kyljen pituus:"),
     window.prompt("Kolmannen kyljen pituus:"));
 
